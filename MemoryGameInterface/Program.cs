bb7ab57f7e0fb7d1ae587a36fffa55a1fca629fa
nirpr <nirpr@mta.ac.il>
2024-06-04 15:44:24 +0300

    added support for more than 2 players and tested the creation of GamePlay object.

--- MemoryGameInterface/Program.cs
+++ MemoryGameInterface/Program.cs
@@ -12,6 +12,15 @@ namespace MemoryGameInterface
     {
         public static void Main()
         {
+            // this Main is just for checks.
+            List<string> lst = new List<string>();
+            lst.Add("Nir");
+            lst.Add("Omer");
+            GamePlay gameplay = new GamePlay(lst, 4, 4);
+            gameplay.firstCellChosenByPlayer(2, 3);
+            gameplay.secondCellChosenByPlayer(1, 2);
+            gameplay.firstCellChosenByPlayer(1, 2);
+            gameplay.secondCellChosenByPlayer(0, 3);
         }
     }
 }
